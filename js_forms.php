94b1ba7e7af8359539c773ce295d59bf8f6b0694
PetkoPopov <poetpoet2014@mail.ru>
2020-11-17 23:58:41 +0200

    някои подобрения по html

--- js_forms.php
+++ js_forms.php
@@ -3,7 +3,6 @@
     <body style="background-color: #669900">
         <form id="myForm">
             a:  <input type="number" name="a"  value="111" style="width:99px;height: 66px;background-color: #ffcc33;text-align: center;font-size: 40px " />
-
             b:<input type="number" name="b"  value="111" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
             c:<input type="number" name="c"  value="111" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/><p></p>
             m:<input type="number" name="m"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
@@ -15,12 +14,13 @@
         <button onclick="takeDataForPiramide()" style="background-color: chocolate;height:55px">get parameters</button>
         <button onclick="main()" style = "background-color: darkslategrey;height: 55px">double click to show diagram</button>
 
-        <a id="hello"></a>
+        <a id="hello" style="font-size:35px;></a>
 
-        <canvas id="myCanvas" height="300" width="300"></canvas>        
+        <canvas id="myCanvas" height="300" width="300" ></canvas>        
 
         <script>
             function main() {
+                var pyramide = [];
 
                 var a = document.getElementById("myForm").elements[0].value;
                 var b = document.getElementById("myForm").elements[1].value;
@@ -30,52 +30,85 @@
                 var l = document.getElementById("myForm").elements[5].value;
 
                 var cos_ab = cosinus(a, b, c);
+                var hlp = "<p>---" + cos_ab + " --->cosinus betwin a and b ----";
+                pyramide.push(hlp);
+
+                let angle = Math.acos(cos_ab);
+                angle *= (180 / 3.1415)
+                angle = Math.round(angle);
+                hlp = "---" + angle + " --->angle  betwin a and b  ----</p>";
+                pyramide.push(hlp);
 
                 var cos_al = cosinus(a, l, n);
+                hlp = "---" + cos_al + "--->cosinus betwin a and l";
+                pyramide.push(hlp);
+
+                angle = Math.acos(cos_al);
+                angle *= (180 / 3.1415)
+                angle = Math.round(angle);
+                hlp = "---" + angle + "---> a  &#8738  l";
+                pyramide.push(hlp);
+
                 var cos_bl = cosinus(b, l, m);
-//                document.getElementById("hello").innerHTML=l+"<p></p>";
+                hlp = "<p>" + cos_bl + "&#8611 cosinus betwin b and l";
+                pyramide.push(hlp);
+
+                angle = Math.acos(cos_bl);
+                angle *= (180 / 3.1415)
+                angle = Math.round(angle);
+                hlp = "---" + angle + "---> &#8738  betwin b and l</p>";
+                pyramide.push(hlp);
+
                 var a_middle = l * cos_al;
+                hlp = '<p style="font-size:35px;">' + Math.round(a_middle) + "&#8614 a middle</p>"
 
+                pyramide.push(hlp);
                 var b_middle = l * cos_bl;
-//                document.getElementById("hello").innerHTML=a_middle+"<p></p>";
                 var c_middle = sideByTwoSidesAndCos(a_middle, b_middle, cos_ab);
-
                 var h_a = hByCosFiAndL(l, cos_al);
+
+                hlp = "<p>" + Math.round
+                (h_a) + '---->h-a</p>';
+                pyramide.push(hlp);
                 var h_b = hByCosFiAndL(l, cos_bl);
+                
+                hlp = "<p>" + Math.round(h_b) + '---->h-b</p>';
+                pyramide.push(hlp);
+                ////////////////////////
                 var hPyramude = hByThreeSides(h_a, h_b, c_middle);
-                var pyramide = [];
-                let hlp = "<a><p>" + hPyramude + "h Pyramide</p></a>";
+                hlp = "<p>" + hPyramude + "-------->h Pyramide</p>";
                 pyramide.push(hlp);
                 var baseArea = areaThreeSides(a, b, c);
-                hlp = "<a><p>" + baseArea + " base ArEA</p></a>";
+                hlp = baseArea + " base area";
                 pyramide.push(hlp);
                 var areaLeftSide = areaThreeSides(a, l, n);
-
-                hlp = "<a><p>" + areaLeftSide + "arae left side</p></a>";
+                hlp = "<p>" + areaLeftSide + "arae left side</p>";
                 pyramide.push(hlp);
                 var areaRightSide = areaThreeSides(b, l, m);
-                hlp = hlp = "<a><p>" + areaRightSide + "area right side</p></a>";
+                hlp = hlp = areaRightSide + "area right side";
                 pyramide.push(hlp);
-
                 var areaBackSide = areaThreeSides(c, m, n);
-                hlp = hlp = "<a><p>" + areaBackSide + "area back side</p></a>";
-
+                hlp = "<p>" + areaBackSide + "area back side</p>";
                 pyramide.push(hlp);
                 document.getElementById("hello").innerHTML = pyramide;
-
             }
+
             /**
              * 
-             * @param {type} a side one 
-             * @param {type} b side two
-             * @param {type} c  side three
+             * @param {type} a kated one 
+             * @param {type} b kated two
+             * @param {type} c  side three hipotenuse
              * @returns {Number}
              */
-            function hByThreeSides(a, b, c) {
-                let cos_ab = cosinus(a, b, c);
-                let h = hByCosFiAndL(a, cos_ab);
-                return h;
-
+            function hByThreeSides($a, $b, $c) {
+                $a = Number($a);
+                $b = Number($b);
+                $c = Number($c);
+                let areaHlp=areaThreeSides($a,$b,$c);
+                let hByOtherWay=2*areaHlp/$c;
+                let $cos_ac=cosinus($a, $c,$b);
+                let h = hByCosFiAndL($a, $cos_ac);
+                return hByOtherWay;
             }
             /**
              * 
@@ -85,6 +118,9 @@
              * @returns {undefined}
              */
             function sideByTwoSidesAndCos(a, b, cos) {
+                a = Number(a);
+                b = Number(b);
+                cos = Number(cos);
                 var c = Math.sqrt(a * a + b * b - cos * 2 * a * b);
                 return c;
             }
@@ -94,9 +130,12 @@
              * @param {number} cos cos f on the angle
              * @returns {Number}
              */
-            function hByCosFiAndL(l, cos) {
+            function hByCosFiAndL(k, cos) {
 
-                var h = Math.sqrt(l * l - (l * cos * l * cos));
+                k = Number(k);
+                cos = Number(cos);
+                let hlp=(k*k)-(k*cos)*(k*cos);
+                var h = Math.sqrt(hlp);
                 return h;
             }
             /**
@@ -108,6 +147,9 @@
              * @returns {Array|calcCoordinateX.coordinates}
              */
             function calcCoordinateX(a, b, c) {
+                a = Number(a);
+                b = Number(b);
+                c = Number(c);
                 let coordinates = [];
                 let x = (c * c - a * a - b * b) / (2 * a);
                 if (x < 0) {
@@ -122,12 +164,11 @@
 
 
                 let y;
-                y = (c * c) - (x * x);//Питагорова теорема
+                y = (c * c) - (x * x); //Питагорова теорема
                 y = Math.sqrt(y);
                 coordinates.push(a);
                 coordinates.push(y);
                 coordinates.push(x);
-
                 return coordinates;
             }
             /**
@@ -138,7 +179,9 @@
              * @returns {Number}
              */
             function areaTwoSidesSinus(a, b, sin) {
-
+                a = Number(a);
+                b = Number(b);
+                sin = Number(sin);
                 return a * b * sin / 2;
             }
             /**
@@ -148,22 +191,22 @@
              * @param {type} c tird side
              * @returns {undefined}
              */
-            function areaThreeSides(a=0,b=0,c=0) {
-                if(a==0&&b==0&&c==0){
-                var $e = takeDataForPiramide();
-                let a =Number($e[0]);
-                let b = Number($e[1]);
-                let c = Number($e[2]);}
-            else{
-        a=Number(a);
-        b= Number(b);
-        c=Number(c);
-        }
+            function areaThreeSides(a = 0, b = 0, c = 0) {
+                if (a == 0 && b == 0 && c == 0) {
+                    var $e = takeDataForPiramide();
+                    let a = Number($e[0]);
+                    let b = Number($e[1]);
+                    let c = Number($e[2]);
+                } else {
+                    a = Number(a);
+                    b = Number(b);
+                    c = Number(c);
+                }
                 let x = 0.25 * (Math.sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a)));
-                x=Math.round(x);
+                x = Math.round(x);
 //                document.getElementById("hello").innerHTML = x
 
-                
+
                 return x;
             }
             /**
@@ -175,7 +218,6 @@
              */
             function cosinus(a, b, c) {
                 let x = (a * a + b * b - c * c) / (2 * a * b);
-
                 return x;
             }
             /**
@@ -193,17 +235,13 @@
                 ctx.lineJoin = 'round';
                 ctx.moveTo(0, 0)
 
-                ctx.lineTo(0, coordinates[0]);//coordinates[0]=a;
+                ctx.lineTo(0, coordinates[0]); //coordinates[0]=a;
 
                 ctx.moveTo(0, coordinates[0]);
                 ctx.lineTo(coordinates[1], coordinates[2]);
-
                 ctx.moveTo(coordinates[1], coordinates[2]);
                 ctx.lineTo(0, 0);
-
-
                 ctx.stroke();
-
             }
 
 
@@ -224,9 +262,7 @@
                 edges.push(m);
                 edges.push(n);
                 edges.push(l);
-
                 var coordinates = calcCoordinateX(a, b, c);
-
                 showDiagram(coordinates);
                 return edges;
             }
