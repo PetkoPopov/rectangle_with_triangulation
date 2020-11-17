8e3db48fd1df1b98d400b46296cddcbf718b0aa6
PetkoPopov <poetpoet2014@mail.ru>
2020-11-17 20:21:25 +0200

    намерени някои грешки в изчисляване на лицето на страните

--- js_forms.php
+++ js_forms.php
@@ -2,18 +2,18 @@
     <title></title>
     <body style="background-color: #669900">
         <form id="myForm">
-            a:  <input type="number" name="a"  value="200" style="width:99px;height: 66px;background-color: #ffcc33;text-align: center;font-size: 40px " />
+            a:  <input type="number" name="a"  value="111" style="width:99px;height: 66px;background-color: #ffcc33;text-align: center;font-size: 40px " />
 
-            b:<input type="number" name="b"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
-            c:<input type="number" name="c"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/><p></p>
+            b:<input type="number" name="b"  value="111" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
+            c:<input type="number" name="c"  value="111" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/><p></p>
             m:<input type="number" name="m"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
             n:<input type="number" name="n"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
             l:<input type="number" name="l"  value="200" style="width:99px;height: 66px;background-color: #ffcc33; text-align: center;font-size: 40px"/>
 
 
         </form>
-        <button onmouseover="takeDataForPiramide()" style="background-color: chocolate;height:55px">get parameters</button>
-        <button onmouseover="main()" style = "background-color: darkslategrey;height: 55px">double click to show diagram</button>
+        <button onclick="takeDataForPiramide()" style="background-color: chocolate;height:55px">get parameters</button>
+        <button onclick="main()" style = "background-color: darkslategrey;height: 55px">double click to show diagram</button>
 
         <a id="hello"></a>
 
@@ -43,7 +43,25 @@
                 var h_a = hByCosFiAndL(l, cos_al);
                 var h_b = hByCosFiAndL(l, cos_bl);
                 var hPyramude = hByThreeSides(h_a, h_b, c_middle);
-                document.getElementById("hello").innerHTML = hPyramude;
+                var pyramide = [];
+                let hlp = "<a><p>" + hPyramude + "h Pyramide</p></a>";
+                pyramide.push(hlp);
+                var baseArea = areaThreeSides(a, b, c);
+                hlp = "<a><p>" + baseArea + " base ArEA</p></a>";
+                pyramide.push(hlp);
+                var areaLeftSide = areaThreeSides(a, l, n);
+
+                hlp = "<a><p>" + areaLeftSide + "arae left side</p></a>";
+                pyramide.push(hlp);
+                var areaRightSide = areaThreeSides(b, l, m);
+                hlp = hlp = "<a><p>" + areaRightSide + "area right side</p></a>";
+                pyramide.push(hlp);
+
+                var areaBackSide = areaThreeSides(c, m, n);
+                hlp = hlp = "<a><p>" + areaBackSide + "area back side</p></a>";
+
+                pyramide.push(hlp);
+                document.getElementById("hello").innerHTML = pyramide;
 
             }
             /**
@@ -83,6 +101,7 @@
             }
             /**
              * a,b,c side of the triangle
+             * изчислява координатите на върховете на триъгълник по дадени дължините на три страни
              * @param {type} a
              * @param {type} b
              * @param {type} c
@@ -90,13 +109,25 @@
              */
             function calcCoordinateX(a, b, c) {
                 let coordinates = [];
-                let x = (a * a + c * c - b * b) / (2 * b);
+                let x = (c * c - a * a - b * b) / (2 * a);
+                if (x < 0) {
+                    x *= -1;
+                    x = a - x;
+                } else {
+
+                    x += a;
+                }
+
+
+
+
                 let y;
-                y = c * c - x * x;
+                y = (c * c) - (x * x);//Питагорова теорема
                 y = Math.sqrt(y);
                 coordinates.push(a);
-                coordinates.push(x);
                 coordinates.push(y);
+                coordinates.push(x);
+
                 return coordinates;
             }
             /**
@@ -117,8 +148,22 @@
              * @param {type} c tird side
              * @returns {undefined}
              */
-            function areaThreeSides(a, b, c) {
-                let x = 0.25 * Math.sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a));
+            function areaThreeSides(a=0,b=0,c=0) {
+                if(a==0&&b==0&&c==0){
+                var $e = takeDataForPiramide();
+                let a =Number($e[0]);
+                let b = Number($e[1]);
+                let c = Number($e[2]);}
+            else{
+        a=Number(a);
+        b= Number(b);
+        c=Number(c);
+        }
+                let x = 0.25 * (Math.sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a)));
+                x=Math.round(x);
+//                document.getElementById("hello").innerHTML = x
+
+                
                 return x;
             }
             /**
@@ -140,23 +185,25 @@
              * @param {type} a    first side of triangle 
              * @returns {undefined}
              */
-            function showDiagram(coordinates, coorRim, a) {
+            function showDiagram(coordinates) {
 
                 var canvas = document.getElementById("myCanvas");
                 var ctx = canvas.getContext("2d");
+                ctx.lineWidth = 2;
+                ctx.lineJoin = 'round';
                 ctx.moveTo(0, 0)
-                ctx.lineTo(0, a);
-                ctx.moveTo(0, a);
-                ctx.lineTo(coordinates[2], coordinates[1]);
-                ctx.moveTo(coordinates[2], coordinates[1]);
+
+                ctx.lineTo(0, coordinates[0]);//coordinates[0]=a;
+
+                ctx.moveTo(0, coordinates[0]);
+                ctx.lineTo(coordinates[1], coordinates[2]);
+
+                ctx.moveTo(coordinates[1], coordinates[2]);
                 ctx.lineTo(0, 0);
-                ctx.moveTo(0, 0)
-                ctx.lineTo(coorRim[0], coorRim[1]);
-                ctx.moveTo(coorRim[0], coorRim[1]);
-                ctx.lineTo(coordinates[2], coordinates[1]);
-                ctx.moveTo(coorRim[0], coorRim[1]);
-                ctx.lineTo(0, a);
+
+
                 ctx.stroke();
+
             }
 
 
@@ -179,8 +226,8 @@
                 edges.push(l);
 
                 var coordinates = calcCoordinateX(a, b, c);
-                var coorRim = calcCoordinateX(a, m, n);
-                showDiagram(coordinates, coorRim, a);
+
+                showDiagram(coordinates);
                 return edges;
             }
 
